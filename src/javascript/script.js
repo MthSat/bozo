24da4d0e30c3e4fabe0445d55213f9133e78503d
Matheus Saturnino <mattsat3@gmail.com>
2025-01-12 04:38:58 -0400

    implementando colorpicker e mudanças nas cores gerais

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -6,12 +6,21 @@ var player1_pontuation = [null,null,null,null,null,null,null,null,null,null];
 var player1_total = 0;
 var player1_plays = 1;
 const inputPlayer1 = document.getElementById('player1');
+var colorPlayer1 = document.getElementById('color-player1');
+var colorPlayer1_Input = document.getElementById('color-player1');
+
+
 
 var player2_name = "";
 var player2_pontuation = [null,null,null,null,null,null,null,null,null,null];
 var player2_total = 0;
 var player2_plays = 1;
 const inputPlayer2 = document.getElementById('player2');
+var colorPlayer2 = document.getElementById('color-player2');
+var colorPlayer2_Input = document.getElementById('color-player2');
+
+
+
 
 var playerTurnName = document.getElementById('playerturn');
 var turn = 0;
@@ -96,6 +105,10 @@ buttonName1.addEventListener('click',()=> {
         player1_name = inputPlayer1.value;
         buttonName1.disabled = 1;
         inputPlayer1.disabled = 1;
+        colorPlayer1_Input.disabled = 1;
+        colorPlayer1 = document.getElementById('color-player1').value;
+        document.documentElement.style.setProperty('--color-player1', String(colorPlayer1)+70);
+
     }
 });
 
@@ -106,7 +119,12 @@ buttonName2.addEventListener('click',()=> {
         player2_name = inputPlayer2.value;
         buttonName2.disabled = 1;
         inputPlayer2.disabled = 1;
+        colorPlayer2_Input.disabled = 1;
+        colorPlayer2 = document.getElementById('color-player2').value;
+        document.documentElement.style.setProperty('--color-player2', String(colorPlayer2)+70);
+
     }
+    
 });
 
 startGame.addEventListener('click',()=> {
@@ -121,6 +139,9 @@ startGame.addEventListener('click',()=> {
         scorePoints.disabled = 1;
         BlockBoard (0);
         addPoints.disabled = 0;
+        document.documentElement.style.setProperty('--board-color', String(colorPlayer1)+70);
+        document.documentElement.style.setProperty('--board-border', "#000000");
+
     }
 });
 
@@ -130,8 +151,10 @@ newGame.addEventListener('click',()=> {
     startGame.disabled = 0;
     inputPlayer1.disabled = 0;
     buttonName1.disabled = 0;
+    colorPlayer1_Input.disabled = 0;
     inputPlayer2.disabled = 0;
     buttonName2.disabled = 0;
+    colorPlayer2_Input.disabled = 0;
     inputPlayer1.value = "";
     player1_name = "";
     inputPlayer2.value = "";
@@ -150,22 +173,29 @@ newGame.addEventListener('click',()=> {
     scorePoints.disabled = 1;
     BlockBoard (1);
     addPoints.disabled = 1;
+    document.documentElement.style.setProperty('--board-color', "#000000");
+    document.documentElement.style.setProperty('--board-border', "#9b9b9b");
+
 });
 
 addPoints.addEventListener('click',()=> {
     if (player1_plays === 11 && player2_plays === 10){
         AddPoints (player2_pontuation);
         BlockBoard (player2_pontuation);
-        playerTurnName.textContent = "Acabaram as jogadas, aguardando o resultado";
+        playerTurnName.textContent = "Aguardando o resultado";
         addPoints.disabled = 1;
         scorePoints.disabled = 0;
         turn = 2;
         BlockBoard (1);
+        document.documentElement.style.setProperty('--board-border', "#9b9b9b");
+
     }
     if (turn === 0) {
         AddPoints (player1_pontuation);
         ShowBoard (player2_pontuation);
         playerTurnName.textContent = "É a vez de " + player2_name + ", jogada "+ player2_plays+"/10";
+        document.documentElement.style.setProperty('--board-color', String(colorPlayer2)+70);
+
         turn = 1;
         player1_plays ++;
 
@@ -173,6 +203,7 @@ addPoints.addEventListener('click',()=> {
         AddPoints (player2_pontuation);
         ShowBoard (player1_pontuation);
         playerTurnName.textContent = "É a vez de " + player1_name + ", jogada "+ player1_plays+"/10";
+        document.documentElement.style.setProperty('--board-color', String(colorPlayer1)+70);
         turn = 0;
         player2_plays ++;
     }
@@ -190,33 +221,50 @@ scorePoints.addEventListener('click',()=> {
     if (player1_total>player2_total){
 
         document.getElementById("result").innerHTML +=
-            "<br><div id=result-win-player1>"+"<strong>"+
-                "Jogo Nº # "+ games + "</strong>" + "<br>" 
-                +player1_name+" VENCEU!"+ "<br>"
-                +player1_name +": "+"<strong>"+ player1_pontuation +" = "+player1_total+"</strong>"+ "<br>"
-                +player2_name +": "+"<strong>"+ player2_pontuation +" = "+player2_total+"</strong>"+ "<br>"
-            +"</div>";
+
+        `<br>
+        <div id=result-win style="background-color: ${String(colorPlayer1)+70}">
+        <strong>Jogo Nº #${games}</strong><br>
+            ${player1_name} VENCEU!<br>
+            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+        </div>`
+
         games ++;
+        scrollToBottom();
     } else if (player1_total<player2_total){
         document.getElementById("result").innerHTML +=
-        "<br><div id=result-win-player2>"+"<strong>"+
-            "Jogo Nº # "+ games + "</strong>" + "<br>" 
-            +player2_name+" VENCEU!"+ "<br>"
-            +player1_name +": "+"<strong>"+ player1_pontuation +" = "+player1_total+"</strong>"+ "<br>"
-            +player2_name +": "+"<strong>"+ player2_pontuation +" = "+player2_total+"</strong>"+ "<br>"
-        +"</div>";
-    games ++;
+
+        `<br>
+        <div id=result-win style="background-color: ${String(colorPlayer2)+70}">
+        <strong>Jogo Nº #${games}</strong><br>
+            ${player2_name} VENCEU!<br>
+            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+        </div>`
+        
+        games ++;
+        scrollToBottom();
     } else {
         document.getElementById("result").innerHTML +=
-        "<br><div id=result-draw>"+"<strong>"+
-            "Jogo Nº # "+ games + "</strong>" + "<br>" 
-            +"EMPATE!"+ "<br>"
-            +player1_name +": "+"<strong>"+ player1_pontuation +" = "+player1_total+"</strong>"+ "<br>"
-            +player2_name +": "+"<strong>"+ player2_pontuation +" = "+player2_total+"</strong>"+ "<br>"
-        +"</div>";
-    games ++;
+
+        `<br>
+        <div id=result-draw>
+        <strong>Jogo Nº #${games}</strong><br>
+            EMPATE!<br>
+            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+        </div>`
+        
+        games ++;
+        scrollToBottom();
     }
 
+    function scrollToBottom() {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: 'smooth'
+        });
 
-
+    }
 });
