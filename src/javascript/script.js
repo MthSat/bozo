4714193878ca7515a8e770fd5e9237e26fa88c0a
Matheus Saturnino <matheus.saturnino@pegaplantao.com.br>
2025-03-27 01:42:20 -0400

    grandes mudanças na maneira como os jogadores são cadastrados, permitindo colocar quantos forem necessários

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,39 +1,75 @@
 
-//jogadores
+// Array que armazenará os jogadores
+const jogadores = [];
 
-var player1_name = "";
-var player1_pontuation = [null,null,null,null,null,null,null,null,null,null];
-var player1_total = 0;
-var player1_plays = 1;
-const inputPlayer1 = document.getElementById('player1');
-var colorPlayer1 = document.getElementById('color-player1');
-var colorPlayer1_Input = document.getElementById('color-player1');
+// Referências aos elementos HTML
+const inputNome = document.getElementById('nomeJogador');
+const colorPicker = document.getElementById('colorPicker');
+const btnAdicionar = document.getElementById('btnAdicionar');
+const listaJogadores = document.getElementById('listaJogadores');
+const totalJogadores = document.getElementById('totalJogadores');
+
+// Evento de clique do botão "OK"
+btnAdicionar.addEventListener('click', () => {
+  const nome = inputNome.value.trim();
+  const cor = colorPicker.value; // cor selecionada
+
+  if (nome !== '') {
+    // Cria um objeto jogador com os campos definidos
+    const jogador = {
+      nome: nome,
+      corSelecionada: cor,
+      pontuacao: Array(10).fill(null),
+      total: 0,
+      jogadas: 0
+    };
+
+    // Adiciona o jogador à lista
+    jogadores.push(jogador);
+
+    // Atualiza a interface para mostrar o jogador cadastrado com a posição
+    adicionarJogadorNaLista(jogador);
+
+    // Atualiza a quantidade total de jogadores exibida
+    totalJogadores.textContent = `Total de Jogadores: ${jogadores.length}`;
+
+    // Limpa o input e reseta o color picker para branco
+    inputNome.value = '';
+    colorPicker.value = "#ffffff";
+    inputNome.focus();
+  }
+});
+
+// Função para atualizar a lista de jogadores na tela com a posição
+function adicionarJogadorNaLista(jogador) {
+  // A posição é o índice do jogador no array + 1
+  const posicao = jogadores.length;
+  const li = document.createElement('li');
+  
+  // Formata o texto com a posição e o nome do jogador
+  li.textContent = `${posicao}º - ${jogador.nome}`;
+  
+  // Define a cor do texto com a cor escolhida
+  li.style.color = jogador.corSelecionada;
+  
+  listaJogadores.appendChild(li);
+}
 
 
 
-var player2_name = "";
-var player2_pontuation = [null,null,null,null,null,null,null,null,null,null];
-var player2_total = 0;
-var player2_plays = 1;
-const inputPlayer2 = document.getElementById('player2');
-var colorPlayer2 = document.getElementById('color-player2');
-var colorPlayer2_Input = document.getElementById('color-player2');
-
-
+//botões
+const startGame = document.getElementById('start');
+const newGame = document.getElementById('newgame');
+const addPoints = document.getElementById('add');
+const scorePoints = document.getElementById('score');
 
 
 var playerTurnName = document.getElementById('playerturn');
 var turn = 0;
 var games = 1;
+var vez = 0;
+var pvez = 0;
 
-//botões
-const newPlayer = document.getElementById('newplayer');
-const startGame = document.getElementById('start');
-const newGame = document.getElementById('newgame');
-const addPoints = document.getElementById('add');
-const scorePoints = document.getElementById('score');
-const buttonName1 = document.getElementById('name1');
-const buttonName2 = document.getElementById('name2');
 
 
 //tabuleiro
@@ -52,16 +88,16 @@ let gereral = document.getElementById('gereral');
 
 function ShowBoard (pontuation){
 
-one.value = pontuation[0];
-two.value = pontuation[1];
-three.value = pontuation[2];
-four.value = pontuation[3];
-five.value = pontuation[4];
-six.value = pontuation[5];
-full.value = pontuation[6];
-straight.value = pontuation[7];
-square.value = pontuation[8];
-general.value = pontuation[9];
+    one.value = pontuation[0];
+    two.value = pontuation[1];
+    three.value = pontuation[2];
+    four.value = pontuation[3];
+    five.value = pontuation[4];
+    six.value = pontuation[5];
+    full.value = pontuation[6];
+    straight.value = pontuation[7];
+    square.value = pontuation[8];
+    general.value = pontuation[9];
 
 }
 
@@ -92,84 +128,32 @@ function AddPoints (pontuation){
     pontuation[7] = straight.value;
     pontuation[8] = square.value;
     pontuation[9] = general.value;
-    }
+}
 
-newPlayer.addEventListener('click',()=> {
-    alert ("Em breve!");
-});
-
-buttonName1.addEventListener('click',()=> {
-    if (inputPlayer1.value===''){
-        alert("Nome do jogador não pode ser vazio!");
-    }else{
-        player1_name = inputPlayer1.value;
-        buttonName1.disabled = 1;
-        inputPlayer1.disabled = 1;
-        colorPlayer1_Input.disabled = 1;
-        colorPlayer1 = document.getElementById('color-player1').value;
-        document.documentElement.style.setProperty('--color-player1', String(colorPlayer1)+70);
-
-    }
-});
-
-buttonName2.addEventListener('click',()=> {
-    if (inputPlayer1.value===''){
-        alert("Nome do jogador não pode ser vazio!");
-    }else{
-        player2_name = inputPlayer2.value;
-        buttonName2.disabled = 1;
-        inputPlayer2.disabled = 1;
-        colorPlayer2_Input.disabled = 1;
-        colorPlayer2 = document.getElementById('color-player2').value;
-        document.documentElement.style.setProperty('--color-player2', String(colorPlayer2)+70);
-
-    }
-    
-});
 
 startGame.addEventListener('click',()=> {
-    if (player1_name===''||player2_name===''){
-        alert("Nome do jogador não pode ser vazio!");
-    }else{
-        newGame.disabled = 0;
-        newPlayer.disabled = 1;
-        startGame.disabled = 1;
-        playerTurnName.textContent = "É a vez de " + player1_name + ", jogada "+ player1_plays+"/10";
-        ShowBoard (player1_pontuation);
-        scorePoints.disabled = 1;
-        BlockBoard (0);
-        addPoints.disabled = 0;
-        document.documentElement.style.setProperty('--board-color', String(colorPlayer1)+70);
-        document.documentElement.style.setProperty('--board-border', "#000000");
+    
+    newGame.disabled = 0;
+    startGame.disabled = 1;
+    scorePoints.disabled = 1;
+    addPoints.disabled = 0;
+    ShowBoard (jogadores[0].pontuacao);
+    BlockBoard (0);
+    playerTurnName.textContent = "É a vez de " + jogadores[0].nome + ", jogada "+ (jogadores[0].jogadas+1)+"/10";
+    document.documentElement.style.setProperty('--board-color', String(jogadores[0].corSelecionada)+70);
+    document.documentElement.style.setProperty('--board-border', "#000000");
 
-    }
+    
 });
 
 newGame.addEventListener('click',()=> {
     newGame.disabled = 1;
-    newPlayer.disabled = 0;
     startGame.disabled = 0;
-    inputPlayer1.disabled = 0;
-    buttonName1.disabled = 0;
-    colorPlayer1_Input.disabled = 0;
-    inputPlayer2.disabled = 0;
-    buttonName2.disabled = 0;
-    colorPlayer2_Input.disabled = 0;
-    inputPlayer1.value = "";
-    player1_name = "";
-    inputPlayer2.value = "";
-    player2_name = "";
-    player1_pontuation = [null,null,null,null,null,null,null,null,null,null];
-    player2_pontuation = [null,null,null,null,null,null,null,null,null,null];
-    player1_total = 0;
-    player2_total = 0;
     playerTurnName.textContent = "Aguardando jogadores...";
     addPoints.disabled = 0;
     turn = 0;
-    player1_plays = 1;
-    player2_plays = 1;
-    ShowBoard (player1_pontuation);
-    BlockBoard (player1_pontuation);
+    //ShowBoard (player1_pontuation);
+    //BlockBoard (player1_pontuation);
     scorePoints.disabled = 1;
     BlockBoard (1);
     addPoints.disabled = 1;
@@ -179,86 +163,112 @@ newGame.addEventListener('click',()=> {
 });
 
 addPoints.addEventListener('click',()=> {
-    if (player1_plays === 11 && player2_plays === 10){
-        AddPoints (player2_pontuation);
-        BlockBoard (player2_pontuation);
+
+    if (turn<(jogadores.length*10)-1){
+       
+        vez = (turn % jogadores.length)
+        pvez = ((turn+1) % jogadores.length)
+                
+        AddPoints (jogadores[vez].pontuacao);
+        jogadores[vez].jogadas++;
+        turn++;
+        ShowBoard (jogadores[pvez].pontuacao);
+        playerTurnName.textContent = "É a vez de " + jogadores[pvez].nome + ", jogada "+ (jogadores[pvez].jogadas+1)+"/10";
+        document.documentElement.style.setProperty('--board-color', String(jogadores[pvez].corSelecionada)+70);
+        document.documentElement.style.setProperty('--board-border', "#000000");
+              
+
+
+    }else{
+        
+        AddPoints (jogadores[pvez].pontuacao);
+        jogadores[pvez].jogadas++;
         playerTurnName.textContent = "Aguardando o resultado";
+        console.log(jogadores);
         addPoints.disabled = 1;
         scorePoints.disabled = 0;
-        turn = 2;
         BlockBoard (1);
         document.documentElement.style.setProperty('--board-color', "#000000");
         document.documentElement.style.setProperty('--board-border', "#9b9b9b");
-    }
-    if (turn === 0) {
-        AddPoints (player1_pontuation);
-        ShowBoard (player2_pontuation);
-        playerTurnName.textContent = "É a vez de " + player2_name + ", jogada "+ player2_plays+"/10";
-        document.documentElement.style.setProperty('--board-color', String(colorPlayer2)+70);
 
-        turn = 1;
-        player1_plays ++;
-
-    } else if (turn === 1) {
-        AddPoints (player2_pontuation);
-        ShowBoard (player1_pontuation);
-        playerTurnName.textContent = "É a vez de " + player1_name + ", jogada "+ player1_plays+"/10";
-        document.documentElement.style.setProperty('--board-color', String(colorPlayer1)+70);
-        turn = 0;
-        player2_plays ++;
     }
+
 });
 
 scorePoints.addEventListener('click',()=> {
     
     scorePoints.disabled = 1;
 
-    for (let i = 0; i < 10; i++){
-        player1_total += Number(player1_pontuation[i]);
-        player2_total += Number(player2_pontuation[i]);
+    for (let i = 0; i < jogadores.length; i++){
+        jogadores[i].total = jogadores[i].pontuacao.reduce((acc, curr) => acc + Number(curr), 0);
+        console.log(jogadores[i].total);
     }
 
-    if (player1_total>player2_total){
+    const maxScore = Math.max(...jogadores.map(j => j.total));
+    const vencedores = jogadores.filter(j => j.total === maxScore);
+    if (vencedores.length > 1) {
+        // Caso de empate: cria uma lista com os nomes dos jogadores empatados
+        const nomesEmpatados = vencedores.map(j => j.nome).join(', ');
+        console.log(`Empate entre: ${nomesEmpatados} com ${maxScore} pontos!`);        
 
-        document.getElementById("result").innerHTML +=
-
-        `<br>
-        <div id=result-win style="background-color: ${String(colorPlayer1)+70}">
-        <strong>Jogo Nº #${games}</strong><br>
-            ${player1_name} VENCEU!<br>
-            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
-            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
-        </div>`
-
-        games ++;
-        scrollToBottom();
-    } else if (player1_total<player2_total){
-        document.getElementById("result").innerHTML +=
-
-        `<br>
-        <div id=result-win style="background-color: ${String(colorPlayer2)+70}">
-        <strong>Jogo Nº #${games}</strong><br>
-            ${player2_name} VENCEU!<br>
-            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
-            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
-        </div>`
-        
-        games ++;
-        scrollToBottom();
     } else {
+        // Caso haja apenas um vencedor
+        const vencedor = vencedores[0];
+        console.log(`O vencedor é ${vencedor.nome} com ${vencedor.total} pontos!`); 
+
         document.getElementById("result").innerHTML +=
 
         `<br>
-        <div id=result-draw>
+        <div id=result-win style="background-color: ${String(jogadores[0].corSelecionada)+70}">
         <strong>Jogo Nº #${games}</strong><br>
-            EMPATE!<br>
-            ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
-            ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+        ${vencedor.nome} VENCEU!<br>
         </div>`
-        
         games ++;
         scrollToBottom();
     }
+
+
+    // if (player1_total>player2_total){
+
+    //     document.getElementById("result").innerHTML +=
+
+    //     `<br>
+    //     <div id=result-win style="background-color: ${String(colorPlayer1)+70}">
+    //     <strong>Jogo Nº #${games}</strong><br>
+    //         ${player1_name} VENCEU!<br>
+    //         ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+    //         ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+    //     </div>`
+
+    //     games ++;
+    //     scrollToBottom();
+    // } else if (player1_total<player2_total){
+    //     document.getElementById("result").innerHTML +=
+
+    //     `<br>
+    //     <div id=result-win style="background-color: ${String(colorPlayer2)+70}">
+    //     <strong>Jogo Nº #${games}</strong><br>
+    //         ${player2_name} VENCEU!<br>
+    //         ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+    //         ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+    //     </div>`
+        
+    //     games ++;
+    //     scrollToBottom();
+    // } else {
+    //     document.getElementById("result").innerHTML +=
+
+    //     `<br>
+    //     <div id=result-draw>
+    //     <strong>Jogo Nº #${games}</strong><br>
+    //         EMPATE!<br>
+    //         ${player1_name}: <strong> ${player1_pontuation} = ${player1_total}</strong><br>
+    //         ${player2_name}: <strong> ${player2_pontuation} = ${player2_total}</strong><br>
+    //     </div>`
+        
+    //     games ++;
+    //     scrollToBottom();
+    // }
 
     function scrollToBottom() {
         window.scrollTo({
